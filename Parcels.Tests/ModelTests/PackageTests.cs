2d5283dc3547d25627d5e29ac45ded1d812a2187
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-09 10:29:39 -0700

    pass tests for setting Height, length, and Weight
    dquote> Co-authored-by Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
    dquote> Co-authored-by Hernan Verar <hernanverar@gmail.com>

--- Parcels.Tests/ModelTests/PackageTests.cs
+++ Parcels.Tests/ModelTests/PackageTests.cs
@@ -14,5 +14,33 @@ namespace Parcels.Tests
       Package newPackage = new Package(1, 2, 3, 4);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
     }
+    [TestMethod]
+    public void PackageConstructor_SetsAWidth_Package()
+    {      
+      int wi = 1;
+      Package newPackage = new Package(wi, 2, 3, 4);
+      Assert.AreEqual (wi, newPackage.Width);
+    }
+    [TestMethod]
+    public void PackageConstructor_SetsAHeight_Package()
+    {      
+      int h = 2;
+      Package newPackage = new Package(1, h, 3, 4);
+      Assert.AreEqual (h, newPackage.Height);
+    }
+    [TestMethod]
+    public void PackageConstructor_SetsALength_Package()
+    {      
+      int l = 3;
+      Package newPackage = new Package(1, 2, l, 4);
+      Assert.AreEqual (l, newPackage.Length);
+    }
+    [TestMethod]
+    public void PackageConstructor_SetsAWeight_Package()
+    {      
+      int we = 4;
+      Package newPackage = new Package(1, 2, 3, we);
+      Assert.AreEqual (we, newPackage.Weight);
+    }
   }
 }
